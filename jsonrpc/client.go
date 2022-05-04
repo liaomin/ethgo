431f54475e6906308b26bf21e1af08406f14cbc0
liaomin <liao37MIN>
2022-05-04 13:41:30 +0800

    添加池子

--- jsonrpc/client.go
+++ jsonrpc/client.go
@@ -1,6 +1,7 @@
 package jsonrpc
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/umbracle/ethgo/jsonrpc/transport"
@@ -8,8 +9,9 @@ import (
 
 // Client is the jsonrpc client
 type Client struct {
-	transport transport.Transport
-	endpoints endpoints
+	// transport transport.Transport
+	transportPool *TransportPool
+	endpoints     endpoints
 }
 
 type endpoints struct {
@@ -33,32 +35,32 @@ func WithHeaders(headers map[string]string) ConfigOption {
 	}
 }
 
-type ClientPool struct {
-	addr string
-	opts []ConfigOption
-	pool sync.Pool
+type TransportPool struct {
+	url     string
+	headers map[string]string
+	pool    sync.Pool
 }
 
-func NewClientPool(addr string, opts ...ConfigOption) *ClientPool {
-	pool := &ClientPool{addr: addr, opts: opts, pool: sync.Pool{
+func NewTransportPool(url string, headers map[string]string) *TransportPool {
+	pool := &TransportPool{url: url, headers: headers, pool: sync.Pool{
 		New: func() interface{} {
 			for true {
-				client, _ := NewClient(addr, opts...)
-				if client != nil {
-					return client
+				transport, _ := transport.NewTransport(url, headers)
+				if transport != nil {
+					return transport
 				}
 			}
-			return nil
+			panic(fmt.Errorf("can‘t get transport:", url))
 		},
 	}}
 	return pool
 }
 
-func (cp *ClientPool) Require() *Client {
-	return cp.pool.Get().(*Client)
+func (cp *TransportPool) Require() transport.Transport {
+	return cp.pool.Get().(transport.Transport)
 }
 
-func (cp *ClientPool) Release(c *Client) {
+func (cp *TransportPool) Release(c transport.Transport) {
 	cp.pool.Put(c)
 }
 
@@ -74,25 +76,30 @@ func NewClient(addr string, opts ...ConfigOption) (*Client, error) {
 	c.endpoints.n = &Net{c}
 	c.endpoints.d = &Debug{c}
 
-	t, err := transport.NewTransport(addr, config.headers)
-	if err != nil {
-		return nil, err
-	}
-	c.transport = t
+	// t, err := transport.NewTransport(addr, config.headers)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// c.transport = t
+	c.transportPool = NewTransportPool(addr, config.headers)
 	return c, nil
 }
 
 // Close closes the transport
 func (c *Client) Close() error {
-	return c.transport.Close()
+	// return c.transport.Close()
+	return nil
 }
 
 // Call makes a jsonrpc call
 func (c *Client) Call(method string, out interface{}, params ...interface{}) error {
-	return c.transport.Call(method, out, params...)
+	pool := c.transportPool
+	transport := pool.Require()
+	defer pool.Release(transport)
+	return transport.Call(method, out, params...)
 }
 
 // SetMaxConnsLimit sets the maximum number of connections that can be established with a host
 func (c *Client) SetMaxConnsLimit(count int) {
-	c.transport.SetMaxConnsPerHost(count)
+	// c.transport.SetMaxConnsPerHost(count)
 }
